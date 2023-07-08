9e9ff992c60ed1289cd718ff5c0dd5c516de73c1
Jiyun Lee <63193220+jiyuneel@users.noreply.github.com>
2023-07-08 19:24:43 +0900

    [Silver 2] Graph Traversal

---
+++ Baekjoon/24479.py
@@ -0,0 +1,28 @@
+import sys
+input=sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    v1, v2 = map(int, input().split())
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+for i in range(1, N + 1):
+    graph[i].sort()
+
+visited = [0] * (N + 1)
+cnt = 0
+def dfs(V):
+    global cnt
+    cnt += 1
+    visited[V] = cnt
+
+    for i in graph[V]:
+        if not visited[i]:
+            dfs(i)
+
+dfs(R)
+for i in range(1, N + 1):
+    print(visited[i])
